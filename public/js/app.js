9dfc40caea7b998aa8f24a521fe5145701a8fdb7
scrumy101 <sha@bsl.com>
2015-03-30 13:51:40 -0400

    March 30th

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,9 @@
      * [Namespacing]
      */
     var Insta = Insta || {};
+
+
+    var pictures = [];
     
     Insta.App = {
 
@@ -14,8 +17,8 @@
         init: function() {
             this.mostRecent();
             this.getData();
-            this.aboutInfo();
             this.mobileNav();
+            this.mapLocations();
         },
 
         /**
@@ -102,37 +105,165 @@
         },
 
         /**
-         * [about view interaction show/hide]
+         * [ render location points based from most recent tags ]
          */
-        aboutInfo: function() {
-            var about = $('.aboutWrap'),
-                btClose = $('#js-closeAbout').find('a'),
-                bt = $('#js-btAbout'),
-                user = localStorage.getItem('user');
+        mapLocations: function(){
+            socket.on('firstShow', function (data) {
 
-            if( user ) {
-                about.removeClass('active');
-            } else {
-                localStorage.setItem('user', 'visited');
-            }
 
-            btClose.on('click', function(e) {
-                e.preventDefault();
-                about.removeClass('active');
-            });
+                console.log(data);
 
-            bt.on('click', function(e) {
-                e.preventDefault();
-                if( !about.hasClass('active') ) {
-                    about.addClass('active');
-                } else {
-                    about.removeClass('active');
+                //loop through all instances with hashtag
+                for (v=0; v < data.firstShow.length; v ++){
+
+                    console.log(data.firstShow[v].user.username);
+                    console.log(data.firstShow[v].location.latitude);
+                    console.log(data.firstShow[v].location.longitude);
+
+                    var img = data.firstShow[v].images.thumbnail.url;
+                    var lat = data.firstShow[v].location.latitude;
+                    var long = data.firstShow[v].location.longitude;
+
+                    pictures.push([img, lat, long, 17]);
+
+
                 }
-            });
+                console.log(pictures);
+                initialize();
 
+
+
+            });
+        }
+    };
+
+
+
+
+    // The following example creates complex markers to indicate pictures near
+// Sydney, NSW, Australia. Note that the anchor is set to
+// (0,32) to correspond to the base of the flagpole.
+
+    function initialize() {
+        var mapOptions = {
+            zoom: 16,
+            center: new google.maps.LatLng(45.406508, -75.723342)
+        }
+        var map = new google.maps.Map(document.getElementById('map-canvas'),
+            mapOptions);
+
+        setMarkers(map, pictures);
+
+        // To style the map
+        var styles = [
+            {
+                stylers: [
+                    // { hue: "#00d4ff" },
+                    // { saturation: 20 }
+
+                    { hue: "#00d4ff" },
+                    { saturation: 20 },
+                    // { lightness: -20 },
+                    { gamma: 1.51 }
+                ]
+            },{
+                featureType: "road",
+                elementType: "labels",
+                stylers: [
+                    { visibility: "on" }
+                ]
+            },{
+                featureType: "water",
+                elementType: "geometry.fill",
+                stylers: [
+                    { color: '#00d4ff' }
+                ]
+            },{
+                featureType: 'landscape.natural',
+                elementType: 'all',
+                stylers: [
+                    { hue: '#00d4ff' },
+                    { gamma: 1.5 }
+                ]
+            }
+        ];
+
+        map.setOptions({styles: styles});
+
+    }
+
+    /**
+     * Data for the markers consisting of a name, a LatLng and a zIndex for
+     * the order in which these markers should display on top of each
+     * other.
+     */
+
+
+    function setMarkers(map, locations) {
+        // Add markers to the map
+
+        // Marker sizes are expressed as a Size of X,Y
+        // where the origin of the image (0,0) is located
+        // in the top left of the image.
+
+        // Origins, anchor positions and coordinates of the marker
+        // increase in the X direction to the right and in
+        // the Y direction down.
+        var image = {
+            url: 'images/cupcake.png',
+            // This marker is 26 pixels wide by 36 pixels tall.
+            size: new google.maps.Size(26, 32),
+            // The origin for this image is 0,0.
+            origin: new google.maps.Point(0,0),
+            // The anchor for this image is the base of the flagpole at 14,36.
+            anchor: new google.maps.Point(14, 32)
+        };
+        // Shapes define the clickable region of the icon.
+        // The type defines an HTML &lt;area&gt; element 'poly' which
+        // traces out a polygon as a series of X,Y points. The final
+        // coordinate closes the poly by connecting to the first
+        // coordinate.
+        var shape = {
+            coords: [1, 1, 1, 20, 18, 20, 18 , 1],
+            type: 'poly'
+        };
+        var markers = [];
+
+        var contentString = [];
+        for (var i = 0; i < locations.length; i++) {
+            var beach = locations[i];
+            var myLatLng = new google.maps.LatLng(beach[1], beach[2]);
+            var marker = new google.maps.Marker({
+                position: myLatLng,
+                map: map,
+                icon: image,
+                shape: shape,
+                title: beach[0],
+                zIndex: beach[3]
+            });
+            markers.push(marker);
+
+            var string = '<div id="content">'+
+                '<img src="' + locations[i][0] + '"></>'+
+                '<p>@ Author</p>'+
+                '</div>'+
+                '</div>';
+
+             contentString.push([string]);
+
+            google.maps.event.addListener(marker, 'click', function(){
+                infowindow.open(map, this);
+            });
         }
 
-    };
+
+        var infowindow = new google.maps.InfoWindow({
+            content: contentString[1][0],
+            maxWidth: 200
+        });
+
+        var markerCluster = new MarkerClusterer(map, markers);
+    }
 
     Insta.App.init();
 
